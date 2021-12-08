4e1addeb290443df25cf773cd92f2c51b5bcf9b9
ajay <ajarvnd07@gmail.com>
2021-12-08 08:24:22 +0530

    commiting demo 2 to branch aj14

---
+++ src/com/gitdemo/DemoForGit2.java
@@ -0,0 +1,25 @@
+package com.gitdemo;
+import java.util.Scanner;
+
+public class DemoForGit2 {
+
+	public static void main(String[] args) {
+		/*Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter the Number : ");
+		int num = sc.nextInt();
+		int sum=0,temp;
+		//Accessing single digit
+		while(num!=0) {
+				temp = num % 10;
+				sum +=(temp*temp*temp);
+				num = num/10;
+				}
+				*/
+				int sum=14;
+		System.out.println(sum);
+				
+			}
+
+	}
+
+
